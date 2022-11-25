0829517c08873f62697a07d0f9dcd6d55e663e10
DIVINE-ENG <dvninyang@gmail.com>
2022-11-25 11:03:39 +0100

    I created a portfolio site

---
+++ New Portfolio/js/script.js
@@ -0,0 +1,26 @@
+// let body = document.getElementById(myBody).value;
+// function changeBg(){
+//   document.getElementById("myBody").style.backgroundColor = "white"
+//   document.getElementById("myBody").style.color = "black"
+
+// }
+
+
+  // function click1(){
+  //   let firsrQuestion = document.getElementById("check").value;
+  //   if (firsrQuestion == "a" || firsrQuestion == "A"){
+  //       document.getElementById("text").innerHTML =
+  //       "Correct! Move to the next"
+  //       document.getElementById("text").style.color =
+  //       "green"
+  //   } 
+  // const loaderContainer =document.querySelector('.loader-container');
+  // window.addEventListener('load', () => {
+  //   // ...
+  // })
+  // window.addEventListener('load', () => {
+  //   loaderContainer.style.display = 'none'
+  // })
+  // window.addEventListener('load', () => {
+  //   loaderContainer.parentElement.removeChild(loaderContainer)
+  // })
